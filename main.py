caa1f16caab1eeb85169661275ed7fad64032940
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2025-04-25 12:39:44 -0300

    Cadastrar novos alunos

--- main.py
+++ main.py
@@ -62,11 +62,12 @@ while continuar == True:
     print("3- Maior média final")
     print("4- Menor média final")
     print("5- Percentual de aprovados")
+    print("6- Cadastrar novos alunos")
     print("------------------------------\n")
 
     menu = int(input("Escolha uma das opções: "))
 
-    while menu < 1 or menu > 5:
+    while menu < 1 or menu > 6:
         print("Escolha inválida")
         menu = int(input("Escolha uma das opções: "))
 
@@ -118,11 +119,50 @@ while continuar == True:
         print("Menor média final:",end="")
         print(menorMediaFinal)
         
-    else:
+    elif menu == 5:
 
         print("Percentual de aprovados:",end="")
         print((numeroAprovados / quantidadeAlunos)*100,"%")
-    
+    else:
+        quantidadeAlunos = int(input("\nDigite a quantidade de alunos para cadastrar: "))
+        for i in range(quantidadeAlunos):
+            boletim = []
+            nomeAluno = input(f"\nDigite o nome do aluno {i+1}: ")
+            notaProvaT = []
+            notaProjetoP = []
+
+            for a in range(2):
+                notaProvaT.append(float(input(f"Nota da prova teórica {a+1} do aluno(a) {nomeAluno}: ")))
+            for a in range(2):
+                notaProjetoP.append(float(input(f"Nota do projeto {a+1} do aluno(a) {nomeAluno}: ")))
+
+            media = []
+            media.append(0.4 * notaProvaT[0] + 0.6 * notaProvaT[1])
+            media.append((notaProjetoP[0] + notaProjetoP[1]) / 2)
+
+            if media[0] >= 5 and media[1] >= 5:
+                mediaFinal = 0.3 * media[1] + 0.7 * media[0]
+            else:
+                if media[1] > media[0]:
+                    mediaFinal = media[0]
+                else:
+                    mediaFinal = media[1]
+
+            if maiorMediaFinal < mediaFinal:
+                maiorMediaFinal = mediaFinal
+            if menorMediaFinal > mediaFinal:
+                menorMediaFinal = mediaFinal
+            
+            if mediaFinal >= 5:
+                numeroAprovados +=1
+            
+            boletim.append(nomeAluno)
+            boletim.append(notaProvaT)
+            boletim.append(notaProjetoP)
+            boletim.append(media)
+            boletim.append(mediaFinal)
+
+            cadastro.append(boletim)
     print()
 
     if input("Quer continuar no sistema? (s/n): ") == "s":
