1f301d330cb02f9eef2514e7ded69dbab6b190ce
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2025-04-25 10:38:34 -0300

    Squashed commit of the following:
    commit 95f52ef6df95eef9544e1e18f233716d644abb0a
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Fri Apr 25 10:38:12 2025 -0300
    
        Finalizado, ENTREGANDO
    
    commit a87d3d593a740418cdfd2d7ae06ac5bd62d7739f
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Fri Apr 25 10:31:32 2025 -0300
    
        Erro de nome invalido na pesquisa
    
    commit a87ab75db131db9a618218639fe2e949296d024f
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Fri Apr 25 09:27:52 2025 -0300
    
        comentarios excluidos
    
    commit d44a7bf694765efefd79e7d6c729fe3e9e847e58
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Fri Apr 25 09:27:01 2025 -0300
    
        tela inicial pronta
    
    commit f78e7c36a0f6e6a59e0041c4b14f5cf37355d19f
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Thu Apr 24 21:56:05 2025 -0300
    
        Finalizado
    
        so falta a tela inicial
    
    commit 7aff783973cb28b038e5debffab823a455f50eba
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Thu Apr 24 11:28:31 2025 -0300
    
        Acabamos de fazer o trabalho
    
    commit 5dda664a36e66afdbe4da47ebd52f0e2a9f227b4
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Wed Apr 23 09:41:15 2025 -0300
    
        Update main.py
    
    commit d0e7d266eb34ea14ee8a326fc4cba845e1103bf1
    Author: João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
    Date:   Wed Apr 16 08:47:41 2025 -0300
    
        Esqueleto do projeto feito

---
+++ main.py
@@ -0,0 +1,131 @@
+print("------------------------------")
+print(" CADASTRO DE NOTAS DE ALUNOS  ")
+print("------------------------------")
+print()
+quantidadeAlunos = int(input("\nDigite a quantidade de alunos para cadastrar: "))
+cadastro = []
+
+maiorMediaFinal = 0
+menorMediaFinal = 10
+
+numeroAprovados = 0
+for i in range(quantidadeAlunos):
+
+    boletim = []
+    nomeAluno = input(f"\nDigite o nome do aluno {i+1}: ")
+    notaProvaT = []
+    notaProjetoP = []
+
+    for a in range(2):
+        notaProvaT.append(float(input(f"Nota da prova teórica {a+1} do aluno(a) {nomeAluno}: ")))
+    for a in range(2):
+        notaProjetoP.append(float(input(f"Nota do projeto {a+1} do aluno(a) {nomeAluno}: ")))
+
+    media = []
+    media.append(0.4 * notaProvaT[0] + 0.6 * notaProvaT[1])
+    media.append((notaProjetoP[0] + notaProjetoP[1]) / 2)
+
+    if media[0] >= 5 and media[1] >= 5:
+        mediaFinal = 0.3 * media[1] + 0.7 * media[0]
+    else:
+        if media[1] > media[0]:
+            mediaFinal = media[0]
+        else:
+            mediaFinal = media[1]
+
+    if maiorMediaFinal < mediaFinal:
+        maiorMediaFinal = mediaFinal
+    if menorMediaFinal > mediaFinal:
+        menorMediaFinal = mediaFinal
+    
+    if mediaFinal >= 5:
+        numeroAprovados +=1
+    
+    boletim.append(nomeAluno)
+    boletim.append(notaProvaT)
+    boletim.append(notaProjetoP)
+    boletim.append(media)
+    boletim.append(mediaFinal)
+    
+
+
+    cadastro.append(boletim)
+
+continuar = True
+while continuar == True:
+    
+    print("\n------------------------------")
+    print("             MENU             ")
+    print("------------------------------")
+    print("1- Boletins")
+    print("2- Pesquisar boletim pelo nome")
+    print("3- Maior média final")
+    print("4- Menor média final")
+    print("5- Percentual de aprovados")
+    print("------------------------------\n")
+
+    menu = int(input("Escolha uma das opções: "))
+
+    while menu < 1 or menu > 5:
+        print("Escolha inválida")
+        menu = int(input("Escolha uma das opções: "))
+
+    print()
+
+    if menu == 1:
+        print("Boletins:")
+        print()
+        for x in cadastro:
+            print("\nNome: ", x[0])
+            print("Nota da prova teórica 1: ", x[1][0])
+            print("Nota da prova teórica 2: ", x[1][1])
+            print("Nota do projeto 1: ", x[2][0])
+            print("Nota do projeto 2: ", x[2][1])
+            print("Média das provas teóricas: ", x[3][0])
+            print("Média dos projetos: ", x[3][1])
+            print("Média Final: ", x[4])
+
+    elif menu == 2:
+
+        print("Pesquisar boletim pelo nome:")
+        pesquisarNome = input("Nome: ")
+        print()
+        pesquisando = True
+        while pesquisando == True:
+            for x in range(quantidadeAlunos):
+                if cadastro[x][0] == pesquisarNome:
+                    print("Nota da prova teórica 1: ", cadastro[x][1][0])
+                    print("Nota da prova teórica 2: ", cadastro[x][1][1])
+                    print("Nota do projeto 1: ", cadastro[x][2][0])
+                    print("Nota do projeto 2: ", cadastro[x][2][1])
+                    print("Média das provas teóricas: ", cadastro[x][3][0])
+                    print("Média dos projetos: ", cadastro[x][3][1])
+                    print("Média Final: ", cadastro[x][4])
+                    pesquisando = False
+                    break
+            if pesquisando:
+                print("Nome inválido, insira um nome cadastrado")
+                pesquisarNome = input("Nome: ")
+                pesquisando = True
+
+    elif menu == 3:
+
+        print("Maior média final:",end="")
+        print(maiorMediaFinal)
+
+    elif menu == 4:
+
+        print("Menor média final:",end="")
+        print(menorMediaFinal)
+        
+    else:
+
+        print("Percentual de aprovados:",end="")
+        print((numeroAprovados / quantidadeAlunos)*100,"%")
+    
+    print()
+
+    if input("Quer continuar no sistema? (s/n): ") == "s":
+        continuar = True
+    else:
+        continuar = False
